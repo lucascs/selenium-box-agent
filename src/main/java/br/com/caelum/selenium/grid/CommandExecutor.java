88fd5eadea89cd529c9e369d1936646aaa8ca070
lucascs <lucascs@caelum-laptop.(none)>
2009-02-03 18:37:15 -0200

    keeping track of started servers. Will not start another server on same port

--- src/main/java/br/com/caelum/selenium/grid/CommandExecutor.java
+++ src/main/java/br/com/caelum/selenium/grid/CommandExecutor.java
@@ -2,13 +2,17 @@ package br.com.caelum.selenium.grid;
 
 import java.lang.reflect.Constructor;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandExecutor {
 
 	private final URLClassLoader classLoader;
-
+	private final List<Server> upServers;
+	
 	public CommandExecutor(URLClassLoader classLoader) {
 		this.classLoader = classLoader;
+		upServers = new ArrayList<Server>();
 	}
 
 	public void execute(String hubURL, String environment, String host, int port) {
@@ -18,10 +22,29 @@ public class CommandExecutor {
 			Constructor<?> constructor = type.getConstructor(String.class, String.class, String.class, String.class);
 			Object remoteControl = constructor.newInstance(hubURL, environment, host, "" + (port));
 			type.getMethod("register").invoke(remoteControl);
-			type.getMethod("launch", String[].class).invoke(remoteControl, (Object)(new String[] { "-port", port + "" }));
+			// Will not start another Selenium Server on the same port
+			if (!isUp(port)) {  
+				type.getMethod("launch", String[].class).invoke(remoteControl, (Object)(new String[] { "-port", port + "" }));
+				upServers.add(new Server(port));
+			}
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
 	}
+	
+	private boolean isUp(int port) {
+		for (Server server : upServers) {
+			if (server.port == port) {
+				return true;
+			}
+		}
+		return false;
+	}
 
+	private static class Server {
+		int port;
+		public Server(int port) {
+			this.port = port;
+		}
+	}
 }
