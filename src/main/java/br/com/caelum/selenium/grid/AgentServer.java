28766f9a87b5e504a7d25c98f838291debc84359
lucascs <lucascs@caelum-laptop.(none)>
2009-02-03 18:35:25 -0200

    better way to copy files

--- src/main/java/br/com/caelum/selenium/grid/AgentServer.java
+++ src/main/java/br/com/caelum/selenium/grid/AgentServer.java
@@ -1,14 +1,12 @@
 package br.com.caelum.selenium.grid;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.channels.FileChannel;
 
 
 public class AgentServer {
@@ -27,16 +25,15 @@ public class AgentServer {
     }
 
 	private static URL urlFor(String x) throws IOException {
-//		return new URL("file://" + x);
 		URL url = AgentServer.class.getResource(x);
 		File jar = File.createTempFile("selenium", ".jar");
-		OutputStream stream = new BufferedOutputStream(new FileOutputStream(jar), 1024000);
-		InputStream inputStream = new BufferedInputStream(url.openStream());
-		while(inputStream.available() > 0) {
-			stream.write(inputStream.read());
-		}
-		stream.close();
-		inputStream.close();
+		
+		FileOutputStream os = new FileOutputStream(jar);
+		FileInputStream is = new FileInputStream(url.getFile());
+		
+		FileChannel in = is.getChannel();
+		
+		in.transferTo(0, in.size(), os.getChannel());
 		return jar.toURI().toURL();
 	}
 }
